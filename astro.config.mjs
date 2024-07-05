c0bb5507c6c8782dfc57e60eebe33216f861e075
verdulife <verdu@live.com>
2024-07-05 12:41:40 +0200

    remove db

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
-import db from "@astrojs/db";
 import netlify from "@astrojs/netlify";
 
 export default defineConfig({
-  integrations: [tailwind(), svelte(), db()],
+  integrations: [tailwind(), svelte()],
   output: "server",
   adapter: netlify()
 });
--- db/seed.ts
+++
@@ -1,7 +0,0 @@
-import { db, PapersTable, PrintsTable } from 'astro:db';
-import { prints, papers } from '@/lib/consts';
-
-export default async function seed() {
-	await db.insert(PrintsTable).values(prints);
-	await db.insert(PapersTable).values(papers);
-}
