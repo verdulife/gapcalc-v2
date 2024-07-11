424eb44011c3a153e3eac15be1e5ace4a93e39aa
verdulife <verdu@live.com>
2024-07-11 10:50:42 +0200

    fix units

--- src/components/PlotterResult.svelte
+++ src/components/PlotterResult.svelte
@@ -13,8 +13,10 @@
     height_value,
     express_value,
     amount_value;
+
   let copied = false;
   let rest_space = 1;
+  let usedHeight = 0;
 
   $: currentPlotter = $Plotters.find((p) => p.id === plotter_value);
   $: express = express_value;
@@ -22,20 +24,18 @@
   $: currentHeight = +height_value || 0;
 
   $: calcPrice = () => {
-    const usable_width = currentPlotter.width_cm - WIDTH_LOSS_CM;
-    const print_width =
-      +amount_value > 1
-        ? currentWidth + (SEPARATOR_CM * +amount_value - 1)
-        : currentWidth;
-    const per_width = Math.floor(usable_width / print_width);
-    const rows = Math.ceil(+amount_value / per_width);
+    const printable_width = currentPlotter.width_cm - WIDTH_LOSS_CM;
+    const item_width =
+      +amount_value > 1 ? currentWidth + SEPARATOR_CM : currentWidth;
+    const items_per_width = Math.floor(printable_width / item_width) || 1;
+    const rows = Math.ceil(+amount_value / items_per_width);
 
-    // AQUI ESTOY
-    const rest_width = usable_width - currentWidth;
-    rest_space = rest_width / per_width;
-    // AQUI ESTOY
+    rest_space =
+      +amount_value % items_per_width === 0
+        ? 0
+        : Math.abs((+amount_value % items_per_width) - items_per_width);
 
-    const usedHeight = currentHeight * rows;
+    usedHeight = currentHeight * rows;
     let price = currentPlotter.price * ((usedHeight + PLOTTER_ADDED_CM) / 100);
 
     if (express) {
@@ -57,20 +57,20 @@
 </script>
 
 <button
-  class="flex flex-col text-right items-end px-6 pt-6"
+  class="flex flex-col text-right items-end px-6 pt-6 gap-2"
   on:click={() => handleCopy(formatPrice(calcPrice()))}
 >
-  <output class="text-6xl {copied && '!text-green-300'}">
+  <output class="text-5xl {copied && '!text-green-300'}">
     {formatPrice(calcPrice())}
   </output>
 
-  <div>
-    <span class="text-gray-500">
-      Usando {currentPlotter.width_cm}x{currentHeight + PLOTTER_ADDED_CM}cm
+  <div class="text-gray-300 text-lg">
+    <span>
+      Usando {currentPlotter.width_cm}x{usedHeight + PLOTTER_ADDED_CM}cm
     </span>
     {#if rest_space > 0}
-      <span class="text-gray-500">
-        <span class="text-gray-500"> - Entra {rest_space} más</span>
+      <span>
+        - Entran {rest_space} más por fila
       </span>
     {/if}
   </div>
